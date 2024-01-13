89e7dddb6356e63a2370ce8384704a1d877ecdad
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2024-01-13 00:36:59 -0800

    controllers

---
+++ controllers/profilesControllers.js
@@ -0,0 +1,69 @@
+const Profile = require("../models/profile");
+
+const fetchProfiles = async (req, res) => {
+  // get all profiles
+  const profiles = await Profile.find();
+  res.json({ profiles: profiles });
+};
+
+const fetchProfile = async (req, res) => {
+  // get the id from the request
+  const profileId = req.params.id;
+
+  // get the profile with this id
+  const profile = await Profile.findById(profileId);
+
+  // respond with it
+  res.json({ profile: profile });
+};
+
+const createProfile = async (req, res) => {
+  // get the sent in data from the request body
+  const { title, body } = req.body;
+
+  // create a new profile with it
+  const profile = await Profile.create({
+    title: title,
+    body: body,
+  });
+  // respond with the created profile
+  res.json({ profiles: profile });
+};
+
+const updateProfile = async (req, res) => {
+  // get the id from the request
+  const profileId = req.params.id;
+
+  // get the sent in data from the request body
+  const title = req.body.title;
+  const body = req.body.body;
+
+  // find the profile with this id and update it's title and body
+  await Profile.findByIdAndUpdate(profileId, {
+    title: title,
+    body: body,
+  });
+
+  const profile = await Profile.findById(profileId);
+  // respond with the updated profile
+  res.json({ profile: profile });
+};
+
+const deleteProfile = async (req, res) => {
+  // get the id from the request
+  const profileId = req.params.id;
+
+  // delete the profile with this id
+  await Profile.deleteOne({ id: profileId });
+
+  // respond with a message
+  res.json({ success: "Profile deleted" });
+};
+
+module.exports = {
+  fetchProfiles,
+  fetchProfile,
+  createProfile,
+  updateProfile,
+  deleteProfile,
+};
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== "production") {
 // Import dependencies
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
-const Profile = require("./models/profile");
+const profilesController = require("./controllers/profilesControllers");
 
 // create express app
 const app = express();
@@ -18,69 +18,11 @@ app.use(express.json());
 connectToDb();
 
 // routing
-app.get("/", (req, res) => {
-  res.json({ Hello: "World" });
-});
-
-app.get("/profile", async (req, res) => {
-  // get all profiles
-  const profiles = await Profile.find();
-  res.json({ profiles: profiles });
-});
-
-app.get("/profile/:id", async (req, res) => {
-  // get the id from the request
-  const profileId = req.params.id;
-
-  // get the profile with this id
-  const profile = await Profile.findById(profileId);
-
-  // respond with it
-  res.json({ profile: profile });
-});
-
-app.post("/profile", async (req, res) => {
-  // get the sent in data from the request body
-  const { title, body } = req.body;
-
-  // create a new profile with it
-  const profile = await Profile.create({
-    title: title,
-    body: body,
-  });
-  // respond with the created profile
-  res.json({ profiles: profile });
-});
-
-app.put("/profile/:id", async (req, res) => {
-  // get the id from the request
-  const profileId = req.params.id;
-
-  // get the sent in data from the request body
-  const title = req.body.title;
-  const body = req.body.body;
-
-  // find the profile with this id and update it's title and body
-  await Profile.findByIdAndUpdate(profileId, {
-    title: title,
-    body: body,
-  });
-
-  const profile = await Profile.findById(profileId);
-  // respond with the updated profile
-  res.json({ profile: profile });
-});
-
-app.delete("/profile/:id", async (req, res) => {
-  // get the id from the request
-  const profileId = req.params.id;
-
-  // delete the profile with this id
-  await Profile.findByIdAndDelete(profileId);
-
-  // respond with a message
-  res.json({ message: "Profile deleted" });
-});
+app.get("/profile", profilesController.fetchProfiles);
+app.get("/profile/:id", profilesController.fetchProfile);
+app.post("/profile", profilesController.createProfile);
+app.put("/profile/:id", profilesController.updateProfile);
+app.delete("/profile/:id", profilesController.deleteProfile);
 
 // start server
 const PORT = process.env.PORT || 3000;
